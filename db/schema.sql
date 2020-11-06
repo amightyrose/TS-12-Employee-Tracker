2d4be7d536fd52c7be24d74461410964753f0d8d
timsilby <69242373+timsilby@users.noreply.github.com>
2020-11-06 11:09:33 +1000

    Updated schema script to include foreign key constraints.

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,40 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(30) NOT NULL,
+	salary DECIMAL NOT NULL,
+	department_id INT NULL,
+	CONSTRAINT fk_dep_role
+		FOREIGN KEY (department_id)
+		REFERENCES department(id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INT NULL,
+	manager_id INT NULL,
+	CONSTRAINT fk_role_emp
+		FOREIGN KEY (role_id)
+		REFERENCES role(id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+	CONSTRAINT fk_empmanager
+		FOREIGN KEY (manager_id)
+		REFERENCES employee(id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+	PRIMARY KEY (id)
 );
